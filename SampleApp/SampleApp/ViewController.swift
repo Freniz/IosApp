bda26e0aca3edd8e2a75a0f5f44f19f986badfea
Abdul Nizam <abdulnizam@freniz.com>
2018-07-05 12:00:44 +0400

    add some functions

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var alertButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.menuFunction()
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func menuFunction()
+    {
+        menuButton.target = revealViewController()
+        menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        revealViewController().rearViewRevealWidth = 275
+        revealViewController().rightViewRevealWidth = 160
+        
+        alertButton.target = revealViewController()
+        alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+        
+        view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+    }
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
