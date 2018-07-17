97475914454c3c7355859aaf375d64fe5701ced5
Abdulnizam <abdulnizam@freniz.com>
2018-07-17 22:18:37 +0400

    db sample testing

---
+++ dbSample/dbSample/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  dbSample
+//
+//  Created by Abdul Nizam Abdul Rahim on 17/07/2018.
+//  Copyright © 2018 Abdul Nizam Abdul Rahim. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class ViewController: UIViewController {
+    
+    var database: Connection!
+    
+    let usersTable = Table("users")
+    let id = Expression<Int>("id")
+    let name = Expression<String>("name")
+    let email = Expression<String>("email")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let fileUrl = documentDirectory.appendingPathComponent("users").appendingPathExtension("sqlite3")
+            let database = try Connection(fileUrl.path)
+            self.database = database
+        } catch {
+            print(error)
+        }
+    }
+
+    @IBAction func createTable() {
+        print("CREATE TAPPED")
+        
+        let createTable = self.usersTable.create { (table) in
+            table.column(self.id, primaryKey: true)
+            table.column(self.name)
+            table.column(self.email, unique: true)
+        }
+        
+        do {
+            try self.database.run(createTable)
+            print("Created Table")
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func insertUser() {
+        print("INSERT TAPPED")
+        let alert = UIAlertController(title: "Insert User", message: nil, preferredStyle: .alert)
+        alert.addTextField { (tf) in tf.placeholder = "Name" }
+        alert.addTextField { (tf) in tf.placeholder = "Email" }
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let name = alert.textFields?.first?.text,
+                let email = alert.textFields?.last?.text
+                else { return }
+            print(name)
+            print(email)
+            
+            let insertUser = self.usersTable.insert(self.name <- name, self.email <- email)
+            
+            do {
+                try self.database.run(insertUser)
+                print("INSERTED USER")
+            } catch {
+                print(error)
+            }
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+
+
+}
+
